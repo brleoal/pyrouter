be49eeff70958e440f6c6d365a339ca44ecb47e5
Bruno Alonso Leon Alata <b.leonalata@protonmail.com>
2020-07-02 22:09:39 -0500

    Update pyrouter.py

--- pyrouter.py
+++ pyrouter.py
@@ -1,4 +1,9 @@
 """
+Reduce GPS estimated position error in route navigation
+Author: Bruno Alonso Leon Alata
+Date: May 14 2020
+Email: b.leonalata@protonmail.com
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
